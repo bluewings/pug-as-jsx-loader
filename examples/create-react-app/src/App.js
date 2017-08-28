42c4f1eb12b1a504324438114fbdd6dd48ec4954
sungwon.cha <mygenie3@naver.com>
2017-08-29 01:00:37 +0900

    add example for issue #2
    https://github.com/bluewings/pug-as-jsx-loader/issues/2
    
    How to define handlers #2

--- examples/create-react-app/src/App.js
+++ examples/create-react-app/src/App.js
@@ -1,13 +1,33 @@
-import React, { Component } from 'react';
+import { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import template from './App.pug';
 
+const characters = [
+  'Darth Vader',
+  'Luke Skywalker',
+  'Leia Organa',
+  'Han Solo',
+  'R2D2',
+  'C3PO',
+  'Obi-Wan Kenobi',
+];
+
 class App extends Component {
+
+  handleClick = (event) => {
+    alert('clicked');
+  }
+
+  handleRemove = (name, index, event) => {
+    alert(name + ' ' + index + ' ' + event.target.tagName);
+  }
+
   render() {
     return template.call(this, {
       // variables
-      logo
+      characters,
+      logo,
     });
   }
 }
