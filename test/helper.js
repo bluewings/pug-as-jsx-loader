fbe0bcef0cc514f7f52677a259e1d6a7fdc42f46
sungwon.cha <mygenie3@naver.com>
2019-10-03 10:04:48 +0900

    support native pug syntax by https://github.com/billypon
    https://github.com/bluewings/pug-as-jsx-utils/pull/4

--- test/helper.js
+++ test/helper.js
@@ -1,6 +1,6 @@
 const pugAsJsxLoader = require('../');
 
-exports.run = function run(input, addOptions = {}) {
+function run(input, addOptions = {}) {
   return new Promise((resolve) => {
     const opt = {
       cacheable: () => {},
@@ -23,4 +23,30 @@ exports.run = function run(input, addOptions = {}) {
 
     resolve({ jsx, output, variables, components });
   });
-};
+}
+
+function toTestArr(b) {
+  const { tests } = b.split(/\n/).reduce((prev, curr) => {
+    let next = { ...prev };
+    if (curr.search(/^@\s*NAME\s*:/) === 0) {
+      const cursor = { name: curr.replace(/^@\s*NAME\s*:/g, '').trim(), input: [], expected: [] };
+      next = { type: null, tests: [...next.tests, cursor], cursor };
+    } else if (curr.search(/^@\s*INPUT\s*:/) === 0) {
+      next = { ...next, type: 'input' };
+    } else if (curr.search(/^@\s*EXPECTED\s*:/) === 0) {
+      next = { ...next, type: 'expected' };
+    } else if (next.type && Array.isArray(next.cursor[next.type])) {
+      next.cursor[next.type] = [...next.cursor[next.type], curr];
+    }
+    return next;
+  }, { type: null, tests: [], cursor: null });
+
+  return tests.map(({ name, input, expected }) => ({
+    name,
+    input: input.join('\n').trim(),
+    expected: expected.join('\n').trim(),
+  }));
+}
+
+module.exports.run = run;
+module.exports.toTestArr = toTestArr;
---
+++ test/pug-syntax.js
@@ -0,0 +1,103 @@
+require('should');
+const { run, toTestArr } = require('./helper');
+
+const tests = toTestArr(`
+@NAME: interpolation
+
+@INPUT:
+div(foo=bar)
+div(onClick=(() => click()))
+div= foo
+div #{bar}
+
+@EXPECTED:
+<>
+  <div foo={bar}></div>
+  <div onClick={() => click()}></div>
+  <div>{foo}</div>
+  <div>{bar}</div>
+</>
+
+
+@NAME: conditional
+
+@INPUT:
+div
+  if foo
+    div bar
+
+  if foo
+    div foo
+  else
+    div bar
+
+  if foo
+    div foo
+  else if bar
+    div bar
+  else
+    div baz
+
+@EXPECTED:
+<div>
+  {foo && <div>bar</div>}
+  {foo ? <div>foo</div> : <div>bar</div>}
+  {foo ? <div>foo</div> : bar ? <div>bar</div> : <div>baz</div>}
+</div>
+
+
+@NAME: each
+
+@INPUT:
+div
+  each value in array
+    div= value
+
+  each value, index in array
+    div(key=index)= value
+
+@EXPECTED:
+<div>
+  {array.map(value => (
+    <div>{value}</div>
+  ))}
+  {array.map((value, index) => (
+    <div key={index}>{value}</div>
+  ))}
+</div>
+
+
+@NAME: case
+
+@INPUT:
+div
+  case value
+    when 'foo'
+      div foo
+    when 'bar': div bar
+    default
+      div baz
+
+@EXPECTED:
+<div>
+  {(() => {
+    switch (value) {
+      case 'foo':
+        return <div>foo</div>
+      case 'bar':
+        return <div>bar</div>
+      default:
+        return <div>baz</div>
+    }
+    return null
+  })()}
+</div>
+`);
+
+describe('pug syntax', () => {
+  tests.forEach(({ name, input, expected }) => {
+    it(name, () => run(input).then((output) => {
+      output.jsx.should.be.eql(expected);
+    }));
+  });
+});
